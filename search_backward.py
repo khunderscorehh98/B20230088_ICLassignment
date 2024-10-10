891ff747e20a6c7e59566dff2e44a64898a6e0ad
khunderscorehh98 <haziqimac@gmail.com>
2024-10-10 15:13:09 +0800

    simplified search backward

--- search_backward.py
+++ search_backward.py
@@ -1,14 +1,31 @@
-def last_greater_backwd(lst, num):
-    # Iterate over the list backwards using reversed index
-    for i in range(len(lst) - 1, -1, -1):
-        if lst[i] >= num:
-            return i
-    # If no element is greater than or equal to num, return None
-    return None
+# Function to find the last index where a number is greater than or equal to the target
+def find_last_greater_or_equal(numbers_list, target_number):
+    last_index = -1  # Start with -1 to show we haven't found anything yet
 
-# Example usage:
-lst = [1, 3, 5, 7, 9, 11]
-num = 6
+    # Go through the list one by one using a for loop
+    for i in range(len(numbers_list)):  # i is the index of each element in the list
+        if numbers_list[i] >= target_number:  # If the current number is bigger or equal to target_number
+            last_index = i  # Remember the index of this number
 
-index = last_greater_backwd(lst, num)
-print(f"The last index where the element is greater than or equal to {num} is: {index}")
+    # If we found a number, return its index, otherwise return None
+    if last_index != -1:
+        return last_index
+    else:
+        return None  # Return None if no match was found
+
+# Input section: ask the user to give a list of numbers
+user_input = input("Enter a list of numbers separated by spaces: ")  
+# Split the input string by spaces and convert each part into an integer to form a list of numbers
+numbers_list = [int(number) for number in user_input.split()]  
+
+# Ask the user to give a target number to compare with the list
+target_number = int(input("Enter the number to compare with the list: "))
+
+# Call the function to get the last index where a number is greater or equal to the target
+index_result = find_last_greater_or_equal(numbers_list, target_number)
+
+# Show the result to the user
+if index_result is not None:
+    print(f"The last index where a number is greater than or equal to {target_number} is: {index_result}")
+else:
+    print(f"No number in the list is greater than or equal to {target_number}.")
